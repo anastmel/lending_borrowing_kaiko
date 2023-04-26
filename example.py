d5697e2f7cdc79b85ca751625542cb5d21f1178a
Anastasia Melachrinos <66743855+anastmel@users.noreply.github.com>
2023-04-26 18:27:41 +0200

    Update example.py

--- example.py
+++ example.py
@@ -10,5 +10,5 @@ quote_assets = ['usd']
 # 1m, 1h, or 1d. Other values are not accepted
 interval = '1d'
 
-df = kk.kaiko_lb_converted(apikey, start_time=start_time, end_time=end_time, protocols=protocols, assets=assets,  quote_assets=quote_assets, interval=interval)
+df = kk.kaiko_lb_markets(apikey, start_time=start_time, end_time=end_time, protocols=protocols, assets=assets,  quote_assets=quote_assets, interval=interval)
 df.to_csv('lb_markets.csv')
